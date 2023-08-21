fa6009171d8d1ac626aa9c1ff4a5e3ef94333703
Dilzo16 <chathuradilshanudawaththa@gmail.com>
2023-08-22 02:14:31 +0530

    news_list

---
+++ lib/news_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NewsPage extends StatefulWidget {
+  const NewsPage({super.key});
+
+  @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
